b4d18e20043610d824d29f06c1c931bd4e7c62a8
Max Sainz <maxsainz2000@gmail.com>
2026-09-18 22:15:50 +0800

    feat: add Windows service installation script and configure webhook secret

--- start_jules_agent.bat
+++ start_jules_agent.bat
@@ -1,6 +1,9 @@
 @echo off
 cd /d "C:\Users\maxsa\Documents\24_7_Agent"
 
+:: Set this to the same secret you use in GitHub!
+set WEBHOOK_SECRET=my-super-secret-123
+
 echo Starting Jules Webhook Listener...
 start "Jules Webhook Listener" cmd /k "python jules_webhook_listener.py"
 
